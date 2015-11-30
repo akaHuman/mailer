3f3908a3c3876b34fdfbc19157cde4410d2a5e12
Shreyash Srivastava <shreyash37@gmail.com>
2015-12-01 00:17:52 +0530

    __cmp__ no longer used in Python 3

--- p2/__init__.py
+++ p2/__init__.py
@@ -1,9 +1,7 @@
 import json
 from urllib import request
 import hashlib
-import functools
 
-@functools.total_ordering
 class FileInfo:
     url = ""
     file_name = ""
@@ -17,7 +15,6 @@ class FileInfo:
         self.file_name = file_name
         self.index = index
         response = request.urlopen(self.url)
-        self.size = response.headers["Content-Length"]
         md5sum = hashlib.md5()
         total_read = 0
         while True:
@@ -26,28 +23,35 @@ class FileInfo:
             if not data:
                 break
             md5sum.update(data)
-
+        self.size = total_read
         self.md5 = md5sum.hexdigest()
         self.file_name_no_ext = file_name.split('.')[1]
 
-    def __cmp__(self, other):
-        if self.file_name_no_ext > other.file_name_no_ext:
-            return 1
+    def __lt__(self, other):
         if self.file_name_no_ext < other.file_name_no_ext:
-            return -1
-        if self.size > other.size:
-            return 1
+            return True
         if self.size < other.size:
-            return -1
-        if self.index > other.index:
-            return 1
+            return True
         if self.index < other.index:
-            return -1
-        return 0
+            return True
+        return False
+
+    def __gt__(self, other):
+        if self.file_name_no_ext > other.file_name_no_ext:
+            return True
+        if self.size > other.size:
+            return True
+        if self.index > other.index:
+            return True
+        return False
+
+    def __eq__(self, other):
+        if self.index == other.index:
+            return True
 
 
 file_info_list = []
-input_json = "[{\"url\": \"http://www.google.com/\", \"_id\": 9845, \"desc\": \"example domain\"}, {\"url\": \"https://www.github.com/\", \"_id\": 1234, \"desc\": \"doselect\"}]"
+input_json = input()
 parsed_json = json.loads(input_json)
 i = 0
 
@@ -60,4 +64,9 @@ for element in parsed_json:
     file_info_list.append(FileInfo(url, file_name, i))
     i += 1
 
-print(file_info_list[0] == file_info_list[1])
+file_info_list.sort()
+password = ""
+for file_info in file_info_list:
+    password += file_info.md5
+
+print(password)
